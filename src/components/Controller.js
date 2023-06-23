3b99c2b547c360ca41a1850eca48239ec8a062e0
Trungdev1611 <103551327+Trungdev1611@users.noreply.github.com>
2023-06-23 09:09:36 +0700

    Create Controller.js

---
+++ src/components/Controller.js
@@ -0,0 +1,34 @@
+import "./styles.css";
+import { Controller, useForm } from 'react-hook-form';
+
+export default function App() {
+  const { control, formState: { errors }, handleSubmit } = useForm();
+
+  const submitFn = (data) => {
+    //được log ra khi lỗi không tồn tại trong errors
+    console.log(`data`, data)
+  };
+  console.log("err", errors);  //khi có lỗi sẽ được in ở đây
+  return (
+    <div className="App">
+  
+        <Controller
+          name="testinput"
+          control={control}
+      
+          rules={{ 
+            required: 'This field is required.',
+            pattern: {
+              value: /^[A-Za-z]+$/,
+              message: 'Only letters are allowed.'
+            }
+           }}
+          render={({ field }) => (
+            <input {...field}    
+          />
+          )}
+        />
+        <button onClick = {handleSubmit(submitFn)}>Submit</button>
+    </div>
+  );
+}
